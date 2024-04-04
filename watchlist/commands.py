9721ffb1eacbc045ecd217888f76372eb653909a
lebron <hbjmailformac@163.com>
2024-04-04 12:07:45 +0800

    pip freeze > requirements.txt

--- watchlist/commands.py
+++ watchlist/commands.py
@@ -62,3 +62,12 @@ def search():
 @app.cli.command()
 def hello():
     print("Hello from custom command!")
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
